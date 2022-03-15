5426fcba3938f1b25db41aecb0254dbd44a4a593
Yonatan Arm <yoniarm123@gmail.com>
2022-03-15 20:08:55 +0200

    k

--- vite.config.js
+++ vite.config.js
@@ -13,16 +13,6 @@ export default defineConfig({
     }
   },
   build:{
-    chunkSizeWarningLimit:1500,
-    rollupOptions: {
-      output:{
-          manualChunks(id) {
-            if (id.includes('node_modules')) {
-                return id.toString().split('node_modules/')[1].split('/')[0].toString();
-            }
-        }
-      }
-  },
     outDir: '../backend/public',
     emptyOutDir: true
   }
